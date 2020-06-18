415b6070eed8396d51b3a5ceec996fe80f2c71ba
yanyo <honma.ryosuke@gmail.com>
2020-06-19 01:55:36 +0900

    [Add] get a cat by id

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Param } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+  @Get(':id')
+  findOne(@Param('id') id: number): string {
+    return `This action returns a #${id} cat`;
   }
 }
